356995294fc0e35bf728c5e3be3811701ac884f8
ArslanYadov <arsyy90@gmail.com>
2022-12-07 02:55:05 +0300

    add view for user registration

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,12 @@
+from api.views import UserRegistrationView
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
+router = DefaultRouter()
+router.register('users', UserRegistrationView)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
 ]
