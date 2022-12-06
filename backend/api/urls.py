992fa6063add5a8e03b4c5c9593a33c96b867765
ArslanYadov <arsyy90@gmail.com>
2022-12-07 01:45:33 +0300

    upd requirements.txt; remove serializers, urls and views from api

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,6 @@
-from api.views import UserGetTokenView, UserRegistrationView, UserViewSet
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register('users', UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('auth/signup/', UserRegistrationView.as_view(), name='signup'),
-    path('auth/token/', UserGetTokenView.as_view(), name='token')
+    
 ]
