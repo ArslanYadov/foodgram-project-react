5515541d1b750f0cfed3533c8893cbd4d4b10f8a
ArslanYadov <arsyy90@gmail.com>
2022-12-06 21:11:39 +0300

    add reciep and tag models; upd settings for media

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -7,19 +7,20 @@ class User(AbstractUser):
     username = models.CharField(
         max_length=150,
         unique=True,
-        verbose_name='Имя пользователя'
+        verbose_name='Юзернейм'
     )
     first_name = models.CharField(
-        max_length=255,
+        max_length=150,
         verbose_name='Имя'
     )
     last_name = models.CharField(
-        max_length=255,
+        max_length=150,
         verbose_name='Фамилия'
     )
     email = models.EmailField(
+        max_length=254,
         unique=True,
-        verbose_name='Электронная почта'
+        verbose_name='Адрес электронной почты'
     )
 
     class Meta:
@@ -32,3 +33,44 @@ class User(AbstractUser):
     
     def __str__(self) -> str:
         return self.username
+
+
+class Recipe:
+    """Модель рецепта."""
+    author = models.ForeignKey(
+        User,
+        related_name='recipes',
+        verbose_name='Автор',
+        on_delete=models.DO_NOTHING
+    )
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название'
+    )
+    image = models.ImageField(
+        upload_to='recipes/images/',
+        verbose_name='Картинка'
+    )
+    text = models.TextField(verbose_name='Описание')
+    ingredients = models.ManyToManyField(verbose_name='Список ингредиентов')
+    tags = models.ManyToManyField(verbose_name='Список id тегов')
+    coocking_time = models.PositiveIntegerField(verbose_name='Время приготовления')
+
+
+class Tag:
+    """Модель тэга."""
+    name = models.CharField(
+        max_length=200,
+        blank=True,
+        null=True,
+        verbose_name='Название'
+    )
+    color = models.CharField(
+        max_length=7,
+        verbose_name='Цвет в HEX'
+    )
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        verbose_name='Уникальный слаг'
+    )
