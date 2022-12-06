680270839e09f2196e5f4bf48eca14a5a8f92561
ArslanYadov <arsyy90@gmail.com>
2022-12-06 23:05:52 +0300

    add urls for apps; insert rest api

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
+    path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
 
