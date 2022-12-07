7616a8ff52b7690a7b6750d2a4da958c9cd3cc29
ArslanYadov <arsyy90@gmail.com>
2022-12-07 16:36:24 +0300

    upd requirements.txt

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -23,7 +23,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djoser',
-    'rest_framework_simplejwt',
     'api',
     'recipes',
     'users'
@@ -107,7 +106,8 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'send_emails')
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
@@ -115,11 +115,6 @@ REST_FRAMEWORK = {
 
 DJOSER = {
     'ACTIVATION_URL': '#/activate/{uid}/{token}'
-}
-
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
