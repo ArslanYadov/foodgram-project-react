0d6eeb77b2da2c8b0f955daec116bba40b76cbb0
ArslanYadov <arsyy90@gmail.com>
2022-12-10 03:02:42 +0300

    add search filter for ingredient viewset with searhing by name regex

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
 from api.serializers import IngredientSerializer, TagSerializer
 from recipes.models import Ingredient, Tag
+from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 
@@ -8,6 +9,8 @@ class IngredientViewSet(ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
     pagination_class = None
+    filter_backends = (SearchFilter,)
+    search_fields = ('$name',)
 
 
 class TagViewSet(ReadOnlyModelViewSet):
