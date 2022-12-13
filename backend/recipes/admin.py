1a872daed0a9afa66746f8a1f96d6add0083a228
ArslanYadov <arsyy90@gmail.com>
2022-12-14 00:59:02 +0300

    ingredient-admount-for-recipe register in admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from recipes.models import Ingredient, IngredientAmountForRecipe, Recipe, Tag
+from recipes.models import (
+    Favorite,
+    Ingredient,
+    IngredientAmountForRecipe,
+    Recipe,
+    Tag
+)
 
+
+admin.site.register(Favorite)
 admin.site.register(Ingredient)
 admin.site.register(IngredientAmountForRecipe)
 admin.site.register(Tag)
