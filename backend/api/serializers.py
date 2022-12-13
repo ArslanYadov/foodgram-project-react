a1a1c48c1ed4e0743d9f5534db84e338e56b69f2
ArslanYadov <arsyy90@gmail.com>
2022-12-14 01:01:13 +0300

    add serializer for ingredient-amount and ingredients_field for recipes list

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,10 @@
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Ingredient, IngredientAmountForRecipe, Recipe, Tag
 from rest_framework import serializers
 from users.serializers import UserDetailSerializer
 
 
 class IngredientSerializer(serializers.ModelSerializer):
+    """Сериализатор для ингредиентов."""
 
     class Meta:
         model = Ingredient
@@ -11,15 +12,28 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 
 class TagSerializer(serializers.ModelSerializer):
+    """Сериализатор для тэгов."""
 
     class Meta:
         model = Tag
         fields = '__all__'
+    
+
+class IngredientAmountForRecipeSerializer(serializers.ModelSerializer):
+    """Сериализатор для ингредиентов с количеством."""
+    id = serializers.ReadOnlyField(source='ingredient.id')
+    name = serializers.ReadOnlyField(source='ingredient.name')
+    measurement_unit = serializers.ReadOnlyField(source='ingredient.measurement_unit')
+
+    class Meta:
+        model = IngredientAmountForRecipe
+        fields = ('id', 'name', 'measurement_unit', 'amount')
 
 
 class RecipesListSerializer(serializers.ModelSerializer):
+    """Сериализатор рецептов для метода GET."""
     author = UserDetailSerializer()
-    ingredients = IngredientSerializer(many=True)
+    ingredients = serializers.SerializerMethodField()
     tags = TagSerializer(many=True)
 
     class Meta:
@@ -28,3 +42,13 @@ class RecipesListSerializer(serializers.ModelSerializer):
             'id', 'tags', 'author', 'ingredients',
             'name', 'image', 'text', 'coocking_time'
         )
+        read_only_fields = ('ingredients',)
+    
+    def get_ingredients(self, obj):
+        queryset = IngredientAmountForRecipe.objects.filter(recipe=obj)
+        return IngredientAmountForRecipeSerializer(queryset, many=True).data
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    """Сериализатор рецептов для методов POST, PATCH и DEL."""
+    pass
