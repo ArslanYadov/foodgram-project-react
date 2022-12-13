0bed425121f9bb5454fa21167843f51f989b2c47
ArslanYadov <arsyy90@gmail.com>
2022-12-13 16:56:10 +0300

    tests for recipes_list and recipe_id urls

--- backend/api/tests/test_urls.py
+++ backend/api/tests/test_urls.py
@@ -1,6 +1,7 @@
 from rest_framework import status
 from rest_framework.test import APITestCase
-from recipes.models import Ingredient, Tag
+from recipes.models import Ingredient, Recipe, Tag
+from users.models import User
 
 
 class TagUrlTests(APITestCase):
@@ -61,8 +62,60 @@ class IngredientUrlTests(APITestCase):
             'name': 'Капуста',
             'measurement_unit': 'кг'
         }
-        ingredient_data = Ingredient.objects.create(**ingredient_data)
+        ingredient = Ingredient.objects.create(**ingredient_data)
 
-        ingredient_id_url = self.ingredient_url + str(ingredient_data.id) + '/'
+        ingredient_id_url = self.ingredient_url + str(ingredient.id) + '/'
         response = self.client.get(path=ingredient_id_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class RecipeUrlTests(APITestCase):
+    """Класс тестов URL адресов для рецептов."""
+    def setUp(self):
+        self.recipe_list_url = 'http://testserver/api/recipes/'
+
+    def test_recipes_list_url(self):
+        """Тест доступности URL со списком рецептов."""
+        response = self.client.get(path=self.recipe_list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_non_exists_recipe_id_url(self):
+        """
+        Тестируем ответ URL по id рецепта,
+        если он ещё не создан.
+        """
+        recipe_id_url = self.recipe_list_url + '1/'
+        response = self.client.get(path=recipe_id_url)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_exists_recipe_id_url(self):
+        """Тестируем ответ URL по id существующего рецепта."""
+        tag_data = {
+            'name': 'завтрак',
+            'color': '#E26C2D',
+            'slug': 'breakfast'
+        }
+        tag = Tag.objects.create(**tag_data)
+        
+        user_data = {
+            'first_name': 'Вася',
+            'last_name': 'Пупкин',
+            'username': 'vasya.pupkin',
+            'email': 'vpupkin@yandex.ru',
+            'password': 'Qwerty_123'
+        }
+        user = User.objects.create(**user_data)
+
+        recipe_data = {
+            'author': user,
+            'image': 'image.jpg',
+            'name': 'Тестовый рецепт',
+            'text': 'Описание тестового рецепта',
+            'coocking_time': 1
+        }
+        recipe = Recipe.objects.create(**recipe_data)
+        recipe.tags.add(tag.id)
+
+        recipe_id_url = self.recipe_list_url + str(recipe.id) + '/'
+        response = self.client.get(path=recipe_id_url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
