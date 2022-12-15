12b9e1d0eddd51f501303853b2695629f0654a11
ArslanYadov <arsyy90@gmail.com>
2022-12-15 19:34:56 +0300

    authenticated user can put recipe in favorite

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,16 @@
 from api.permissions import IsAuthorAdminModerOrReadOnly
 from api.serializers import (
     IngredientSerializer,
+    FavoriteSerializer,
     RecipesListSerializer,
     RecipeSerializer,
     TagSerializer
 )
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from recipes.models import Ingredient, Recipe, Tag
-from rest_framework import filters, viewsets, permissions
+from recipes.models import Favorite, Ingredient, Recipe, Tag
+from rest_framework import filters, viewsets, permissions, response, status
+from rest_framework.decorators import action
 
 
 class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
@@ -37,3 +40,22 @@ class RecipeViewSet(viewsets.ModelViewSet):
         if self.request.method in ['GET']:
             return RecipesListSerializer
         return RecipeSerializer
+
+    @action(
+        methods=['POST'], detail=True,
+        permission_classes=[permissions.IsAuthenticated]
+    )
+    def favorite(self, request, pk):
+        data = {'user': request.user.id, 'recipe': pk}
+        serializer = FavoriteSerializer(data=data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    @favorite.mapping.delete
+    def delete_favorite(self, request, pk):
+        user = request.user
+        recipe = get_object_or_404(Recipe, pk=pk)
+        obj = get_object_or_404(klass=Favorite, user=user, recipe=recipe)
+        obj.delete()
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
