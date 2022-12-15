eda85e591bbe048f135dcb11b8891e4ec558cf7c
ArslanYadov <arsyy90@gmail.com>
2022-12-15 20:09:25 +0300

    private methods for create and delete action + user can add/delete recipe in shopping cart

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,13 +2,14 @@ from api.permissions import IsAuthorAdminModerOrReadOnly
 from api.serializers import (
     IngredientSerializer,
     FavoriteSerializer,
+    ShoppingCartSerializer,
     RecipesListSerializer,
     RecipeSerializer,
     TagSerializer
 )
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from recipes.models import Favorite, Ingredient, Recipe, Tag
+from recipes.models import Favorite, Ingredient, Recipe, Tag, ShoppingCart
 from rest_framework import filters, viewsets, permissions, response, status
 from rest_framework.decorators import action
 
@@ -41,21 +42,46 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return RecipesListSerializer
         return RecipeSerializer
 
+    def _create_action(self, request, pk, serializer):
+        data = {'user': request.user.id, 'recipe': pk}
+        serializer = serializer(data=data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return response.Response(
+            serializer.data, status=status.HTTP_201_CREATED
+        )
+
+    def _delete_action(self, request, pk, klass):
+        user = request.user
+        recipe = get_object_or_404(Recipe, pk=pk)
+        obj = get_object_or_404(klass=klass, user=user, recipe=recipe)
+        obj.delete()
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
+
     @action(
         methods=['POST'], detail=True,
         permission_classes=[permissions.IsAuthenticated]
     )
     def favorite(self, request, pk):
-        data = {'user': request.user.id, 'recipe': pk}
-        serializer = FavoriteSerializer(data=data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return response.Response(serializer.data, status=status.HTTP_201_CREATED)
-    
+        return self._create_action(
+            request=request, pk=pk, serializer=FavoriteSerializer
+        )
+
     @favorite.mapping.delete
     def delete_favorite(self, request, pk):
-        user = request.user
-        recipe = get_object_or_404(Recipe, pk=pk)
-        obj = get_object_or_404(klass=Favorite, user=user, recipe=recipe)
-        obj.delete()
-        return response.Response(status=status.HTTP_204_NO_CONTENT)
+        return self._delete_action(request=request, pk=pk, klass=Favorite)
+
+    @action(
+        methods=['POST'], detail=True,
+        permission_classes=[permissions.IsAuthenticated]
+    )
+    def shopping_cart(self, request, pk):
+        return self._create_action(
+            request=request, pk=pk, serializer=ShoppingCartSerializer
+        )
+
+    @shopping_cart.mapping.delete
+    def delete_shopping_cart(self, request, pk):
+        return self._delete_action(
+            request=request, pk=pk, klass=ShoppingCart
+        )
