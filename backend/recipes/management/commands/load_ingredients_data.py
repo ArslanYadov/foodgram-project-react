053559684589c912a64af4d49d64b58f81ccf461
ArslanYadov <arsyy90@gmail.com>
2022-12-19 19:56:01 +0300

    using join method for path to file

--- backend/recipes/management/commands/load_ingredients_data.py
+++ backend/recipes/management/commands/load_ingredients_data.py
@@ -1,4 +1,5 @@
 import json
+import os
 from django.core.management import BaseCommand
 from tqdm import tqdm
 from typing import Any, Optional
@@ -11,7 +12,8 @@ class Command(BaseCommand):
     игнредиентов из json файла.
     """
     filename: str = 'ingredients'
-    file: str = 'static/data/' + filename + '.json'
+    ext: str = '.json'
+    path_to_file: str = os.path.join('static', 'data', filename + ext)
 
     def handle(self, *args: Any, **options: Any) -> Optional[str]:
         if Ingredient.objects.exists():
@@ -22,7 +24,7 @@ class Command(BaseCommand):
             )
             return
 
-        with open(self.file, 'rb') as fin:
+        with open(self.path_to_file, 'rb') as fin:
             data: list[dict[str, str]] = json.load(fin)
 
             for entry in tqdm(data):
