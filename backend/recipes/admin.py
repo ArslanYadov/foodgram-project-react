f2c5de62ea5d87eb863e5efc99bc938434f84d47
ArslanYadov <arsyy90@gmail.com>
2022-12-20 03:41:01 +0300

    add ingredient choice field in recipe admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -23,6 +23,12 @@ class IngredientAdmin(admin.ModelAdmin):
     search_fields = ('name', )
 
 
+class IngredientInline(admin.StackedInline):
+    """Поле с выбором ингредиента и ввода его количества."""
+    model = IngredientAmountForRecipe
+    extra = 0
+
+
 @admin.register(IngredientAmountForRecipe)
 class IngredientAmountForRecipeAdmin(admin.ModelAdmin):
     """Класс админки для количества ингредиента в рецепте."""
@@ -34,9 +40,35 @@ class IngredientAmountForRecipeAdmin(admin.ModelAdmin):
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     """Класс админки рецептов."""
+    inlines = (IngredientInline, )
+    fields = (
+        'author',
+        'name',
+        'image',
+        'text',
+        'ingredient_inline',
+        'tags',
+        'cooking_time'
+    )
+
     list_display = ('name', 'author', 'amount_favorite')
     list_filter = ('author', 'name', 'tags')
     search_fields = ('author__username', 'name__icontains')
+
+    readonly_fields = ('ingredient_inline', )
+
+    @admin.display(description='Ингредиенты')
+    def ingredient_inline(self, *args, **kwargs):
+        from django.template.loader import get_template
+        context = getattr(self.response, 'context_data', None) or {}
+        context['inline_admin_formset'] = context['inline_admin_formsets'].pop(0)
+        inline = context['inline_admin_formset']
+        return get_template(inline.opts.template).render(context, self.request)
+
+    def render_change_form(self, request, *args, **kwargs):
+        self.request = request
+        self.response = super().render_change_form(request, *args, **kwargs)
+        return self.response
 
     @admin.display(description='Количество в избранном')
     def amount_favorite(self, obj):
