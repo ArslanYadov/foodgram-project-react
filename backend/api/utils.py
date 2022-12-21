17e9e6ca482b27637dd847be40a48346c6805fb9
ArslanYadov <arsyy90@gmail.com>
2022-12-21 21:18:42 +0300

    add method for format footer in shopping list

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,4 +1,5 @@
 import base64
+from datetime import datetime
 from django.core.files.base import ContentFile
 from rest_framework.serializers import ImageField
 
@@ -11,3 +12,10 @@ class Base64ImageField(ImageField):
             ext = format.split('/')[-1]
             data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
         return super().to_internal_value(data)
+
+
+def get_shopping_cart_footer() -> str:
+    """Возвращает подвал для вывода списка покупок."""
+    time_format_message: str = 'Список создан в %H:%M от %d/%m/%Y'
+    separate: str = '-' * len(time_format_message)
+    return separate + '\n' + datetime.now().strftime(time_format_message)
