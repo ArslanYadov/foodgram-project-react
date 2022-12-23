78cb394e883d6a4b2cf10a5f43311397e7388ccf
ArslanYadov <arsyy90@gmail.com>
2022-12-23 22:27:05 +0300

    oneline return for subscribed

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -26,13 +26,13 @@ class UserDetailSerializer(UserSerializer):
 
     def get_is_subscribed(self, obj):
         user = self.context.get('request').user
-        if not user.is_authenticated:
-            return False
-        return user.follower.filter(author=obj).exists()
-        # return (
-        #     user.follower.filter(author=obj).exists()
-        #     and user.is_authenticated
-        # )
+        # if not user.is_authenticated:
+        #     return False
+        # return user.follower.filter(author=obj).exists()
+        return (
+            user.follower.filter(author=obj).exists()
+            and user.is_authenticated
+        )
 
 
 class UserRegistrationSerializer(UserCreateSerializer):
